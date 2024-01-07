1209802628026e18e31e5edb1df6c981f7bbd08c
Bhavy <bhavypdevani31@gmail.com>
2024-01-07 22:52:57 +0530

    Create Sign-In page and change bg

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const SignIn = () => {
   return (
-    <p>SignIn</p>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="email" placeholder='Email' className=' text-lg border rounded-xl p-3 hover:shadow-md' id='email' />
+        <input type="password" placeholder='Password' className=' text-lg border rounded-xl p-3 hover:shadow-md' id='password' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70'>
+          Sign In
+        </button>
+        <button
+          className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+        >
+          Continue with google
+        </button>
+      </form>
+      <div className='flex gap-2 mt-4'>
+        <p> Forgot password?</p>
+        <Link to={'/forgot-pass'}>
+          <span className='text-blue-700'>Click here</span>
+        </Link>
+      </div>
+    </div >
   )
 }
 
