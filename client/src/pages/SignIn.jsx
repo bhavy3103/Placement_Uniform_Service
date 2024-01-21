9f59f8acfe1dfbaffbc867357f1ea293e47e61c7
abhijivani3001 <abhijivani3001@gmail.com>
2024-01-21 19:59:37 +0530

    implemented shadcn library

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,26 @@
-import { Link } from "react-router-dom"
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form className='flex flex-col gap-4'>
-        <input type="username" placeholder='Enrollment' className=' text-lg border rounded-xl p-3 hover:shadow-md' id='enrollment' />
-        <input type="password" placeholder='Password' className=' text-lg border rounded-xl p-3 hover:shadow-md' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70'>
-          Sign In
-        </button>
+        <input
+          type='username'
+          placeholder='Enrollment'
+          className=' text-lg border rounded-xl p-3 hover:shadow-md'
+          id='enrollment'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className=' text-lg border rounded-xl p-3 hover:shadow-md'
+          id='password'
+        />
+        <Button>Sign in</Button>
       </form>
-      <div className="flex flex-row justify-between">
+      <div className='flex flex-row justify-between'>
         {/* <div className='flex gap-2 mt-5'>
           <p>Don't have an account?</p>
           <Link to={'/sign-up'}>
@@ -24,8 +33,8 @@ const SignIn = () => {
           </Link>
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
