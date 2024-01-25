14426c82a346b71a3f4d64d96a5949c04d6f11c0
Bhavy <bhavypdevani31@gmail.com>
2024-01-25 11:14:40 +0530

    Add reduxjs/toolkit and complete signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,21 +3,42 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useState } from 'react';
 import AxiosUrl from '../../api/AxiosUrl';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 const SignIn = () => {
   const [enrollment, setEnrollment] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.user);
+
 
   const testApi = async (e) => {
     e.preventDefault();
     try {
+      dispatch(signInStart());
       const res = await AxiosUrl.post('/api/auth/signin', {
         enrollment: enrollment,
         password: password,
       });
-      console.log(res);
+      if (res.status !== 200) {
+        dispatch(signInFailure(res.message));
+        return;
+      }
+      dispatch(signInSuccess(res));
+      navigate('/');
+
     } catch (error) {
-      console.error(error.message);
+      const Credential = "Wrong Credential";
+      dispatch(signInFailure(Credential));
+      // console.log(error.message);
     }
   };
 
@@ -26,11 +47,15 @@ const SignIn = () => {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form className='flex flex-col gap-4'>
         <input
-          type='username'
+          type='number'
           placeholder='Enrollment'
           className=' text-lg border rounded-xl p-3 hover:shadow-md'
           id='enrollment'
           onChange={(e) => {
+            const inputValue = e.target.value;
+            if (!isNaN(inputValue)) {
+              setEnrollment(inputValue);
+            }
             setEnrollment(e.target.value);
           }}
         />
@@ -44,7 +69,9 @@ const SignIn = () => {
             setPassword(e.target.value);
           }}
         />
-        <Button onClick={testApi}>Sign in</Button>
+        <Button onClick={testApi}>
+          {loading ? 'Loading...' : 'Sign In'}
+        </Button>
       </form>
       <div className='flex flex-row justify-between'>
         {/* <div className='flex gap-2 mt-5'>
@@ -59,6 +86,7 @@ const SignIn = () => {
           </Link>
         </div>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 };
