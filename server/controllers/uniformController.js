c680b79e7b13fb90f4e40094e32f34f1467d8468
Bhavy <bhavypdevani31@gmail.com>
2024-02-04 00:00:51 +0530

    update uniform issue by student api route done

--- server/controllers/uniformController.js
+++ server/controllers/uniformController.js
@@ -19,3 +19,27 @@ export const updateStudentUniform = async (req, res) => {
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 };
+
+export const updateUniformIssue = async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const { isIssue } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+    if (isIssue === "No issue found") {
+      user.status = "completed";
+      user.uniform.isIssue = isIssue;
+    }
+    else {
+      user.uniform.isIssue = isIssue;
+      user.status = "pending";
+    }
+    await user.save();
+
+    return res.status(200).json({ success: true, message: "isIssue field updated successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
--- server/routes/uniformRoutes.js
+++ server/routes/uniformRoutes.js
@@ -3,13 +3,14 @@ import {
   authMiddleware,
   authAdminMiddleware,
 } from "../middlewares/authMiddleware.js";
-import { updateStudentUniform } from '../controllers/uniformController.js';
+import { updateStudentUniform, updateUniformIssue } from '../controllers/uniformController.js';
 
 const router = express.Router();
 
 // router.get('/details/:id', authAdminMiddleware, getUserWithUniform);
 
 router.post('/updateUniform', authAdminMiddleware, updateStudentUniform);
+router.patch('/updateIssue', authMiddleware, updateUniformIssue);
 
 
 export default router;
