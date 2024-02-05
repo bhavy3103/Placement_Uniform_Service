4f08ac03170a5913ec6e361cb41345cbdd637dbf
Bhavy <bhavypdevani31@gmail.com>
2024-02-05 08:54:07 +0530

    solved SignIn.Jsx error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/SignIn.Jsx';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Navbar from './components/shared/Navbar';
