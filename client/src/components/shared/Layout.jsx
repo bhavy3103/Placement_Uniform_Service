86c577de6176e35d561fa294f9207a5d783dc81d
abhijivani3001 <abhijivani3001@gmail.com>
2024-05-02 12:38:44 +0530

    added modal for select students to send message

--- client/src/components/shared/Layout.jsx
+++ client/src/components/shared/Layout.jsx
@@ -5,7 +5,7 @@ export const Layout = ({ children }) => {
   return (
     <div className='flex h-[91vh]'>
       <Sidebar />
-      <div className='flex-1 overflow-y-auto m-3 border rounded-lg shadow-lg p-4'>
+      <div className='flex-1 bg-white overflow-y-auto m-3 border rounded-lg shadow-lg p-4'>
         {children}
       </div>
     </div>
--- client/src/pages/Chats.jsx
+++ client/src/pages/Chats.jsx
@@ -2,10 +2,25 @@ import { useState, useEffect } from 'react';
 import Layout from '@/components/shared/Layout';
 import AxiosUrl from '../../api/AxiosUrl';
 import Message from '@/components/shared/Message';
+import { Button } from '@/components/ui/button';
+import { ChevronRight } from 'lucide-react';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '@/components/ui/dialog';
+import StudentList from './StudentList';
 
 const Chats = () => {
   const [message, setMessage] = useState('');
   const [allMessages, setAllMessages] = useState([]);
+  const [selectedEnrollments, setSelectedEnrollments] = useState([]);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -18,8 +33,9 @@ const Chats = () => {
         const res = await AxiosUrl.post('/api/chats/postMessage', {
           message: message,
           timestamp: new Date(),
+          selectedEnrollments: selectedEnrollments,
         });
-        // console.log('res', res.data);
+        // console.log('res', res);
         setMessage('');
         setAllMessages((prev) => [...prev, message]);
       } catch (error) {
@@ -60,44 +76,68 @@ const Chats = () => {
 
   return (
     <Layout>
-      <div className='h-full flex overflow-hidden flex-col justify-between relative'>
-        <div className='overflow-auto' id='main-chat'>
-          {allMessages.length === 0 && (
-            <div className='text-gray-800 text-center overflow-auto px-4 py-3 rounded-lg mt-2'>
-              No messages yet.
-            </div>
-          )}
-          {allMessages?.map((item, index) => (
-            <Message
-              key={index}
-              message={item.message}
-              timestamp={item.timestamp}
-            />
-          ))}
-        </div>
+      <Dialog>
+        <DialogContent className='flex flex-col justify-between sm:max-w-[80vw] w-[70rem] min-h-[80vh] max-h-[90vh] overflow-hidden'>
+          <DialogHeader className=''>
+            <DialogTitle>Select Students</DialogTitle>
+            <DialogDescription>
+              Select the students you want to send this message.
+            </DialogDescription>
+          </DialogHeader>
+          {/* body */}
+          <StudentList
+            selectedEnrollments={selectedEnrollments}
+            setSelectedEnrollments={setSelectedEnrollments}
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type='submit' onClick={handleSubmit}>
+                Send Message
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
 
-        <form
-          onSubmit={handleSubmit}
-          className='sticky bottom-0 w-full flex items-center justify-between gap-4 mt-3'
-        >
-          <textarea
-            id='textarea-message'
-            name='message'
-            rows='1'
-            className='flex-grow bg-gray-200 text-gray-800 focus:outline-none resize-none w-full rounded-md text-md px-4 py-3'
-            placeholder='Type your message here'
-            value={message}
-            onChange={handleMessageChange}
-            onKeyDown={handleKeyPress}
-          ></textarea>
-          <button
-            type='submit'
-            className='bg-gray-700 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2'
+        <div className='h-full flex overflow-hidden flex-col justify-between relative'>
+          <div className='overflow-auto' id='main-chat'>
+            {allMessages.length === 0 && (
+              <div className='text-gray-800 text-center overflow-auto px-4 py-3 rounded-lg mt-2'>
+                No messages yet.
+              </div>
+            )}
+            {allMessages?.map((item, index) => (
+              <Message
+                key={index}
+                message={item.message}
+                timestamp={item.timestamp}
+              />
+            ))}
+          </div>
+
+          <form
+            // onSubmit={handleSubmit}
+            className='sticky bottom-0 w-full flex items-center justify-between gap-4 mt-3'
           >
-            Send
-          </button>
-        </form>
-      </div>
+            <textarea
+              id='textarea-message'
+              name='message'
+              rows='1'
+              className='flex-grow bg-gray-200 text-gray-800 focus:outline-none resize-none w-full rounded-md text-md px-4 py-3'
+              placeholder='Type your message here'
+              value={message}
+              onChange={handleMessageChange}
+              onKeyDown={handleKeyPress}
+            ></textarea>
+
+            <DialogTrigger asChild>
+              <Button className='flex justify-between items-center gap-2 bg-gray-800 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2'>
+                <span>Send</span>
+                <ChevronRight size={24} />
+              </Button>
+            </DialogTrigger>
+          </form>
+        </div>
+      </Dialog>
     </Layout>
   );
 };
